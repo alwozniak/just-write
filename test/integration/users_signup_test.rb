a5294d31d27b554be38d7c7f80bd66ad84b14986
Aleksandra Wozniak <aleksandra.k.wozniak@gmail.com>
2015-02-28 22:02:30 +0000

    Handling successful registration.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
   test "invalid user information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,5 +12,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "pass21"}
     end
     assert_template 'users/new'
+  end
+
+  test "valid user information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {
+        name: "Name",
+        email: "email@email.com",
+        password: "alamakota",
+        password_confirmation: "alamakota"
+      }
+    end
+    assert_template 'users/show'
   end
 end
